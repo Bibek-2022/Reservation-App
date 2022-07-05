92d22f4def6eaa61eacaf379e627426a602f1ead
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-07-05 19:17:55 +1000

    User schema

---
+++ api/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const HotelSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  photos: {
+    type: String,
+    required: true,
+  },
+});
