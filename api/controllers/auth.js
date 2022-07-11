815e66239850942f33e3a117ad52be088194e494
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-07-11 20:26:44 +1000

    Implemented login route and controller

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -16,3 +16,22 @@ export const register = async (req, res, next) => {
     next(error);
   }
 };
+
+// login
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User not found"));
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect) return next(createError(404, "Wrong Password"));
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res.status(200).json(...otherDetails);
+  } catch (error) {
+    next(error);
+  }
+};
