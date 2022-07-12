3c147b68f97fcbcbc93dcf33ce71d095580d8958
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-07-12 20:29:22 +1000

    created user router

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,4 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
+// UPDATE
+router.put("/:id", updateUser);
+// DELETE
+router.delete("/:id", deleteUser);
+// GET
+router.get("/:id", getUser);
+// ALL
+router.get("/", getUsers);
 export default router;
