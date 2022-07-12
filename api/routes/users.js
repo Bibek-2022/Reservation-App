cffc88676a5179986c3bcf0ffbc3674a953ec4df
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-07-12 21:15:21 +1000

    Token verification to user login

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,16 @@
 import express from "express";
-
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("Hello user, you are in");
+});
 
 // UPDATE
 router.put("/:id", updateUser);
