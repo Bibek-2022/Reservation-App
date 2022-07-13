11375ea811a2e9109918d032c25ffd5d223f9be6
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-07-13 22:37:22 +1000

    implemented validation on hotel

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -7,14 +7,15 @@ import {
   updateHotel,
 } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
+import { verifyAdmin } from "../utils/verifyTokens.js";
 
 const router = express.Router();
 // CREATE
-router.post("/", createHotel);
+router.post("/", verifyAdmin, createHotel);
 // UPDATE
-router.put("/:id", updateHotel);
+router.put("/:id", verifyAdmin, updateHotel);
 // DELETE
-router.delete("/:id", deleteHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
 // GET
 router.get("/:id", getHotel);
 // ALL
--- api/routes/users.js
+++ api/routes/users.js
@@ -8,18 +8,18 @@ import {
 import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 const router = express.Router();
 
-router.get("/checkauthentication", verifyToken, (req, res, next) => {
-  res.send("Hello user, you are in");
-});
-router.get("/checkuser/:id", verifyToken, (req, res, next) => {
-  res.send("Hello user, you are in");
-});
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   res.send("Hello user, you are in");
+// });
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res.send("Hello user, you are in");
+// });
 // UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 // DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 // GET
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 // ALL
-router.get("/", getUsers);
+router.get("/", verifyAdmin, getUsers);
 export default router;
