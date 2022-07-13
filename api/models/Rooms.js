d3f3d11091235b55f4bcc625293a9ec7d4bad557
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-07-13 22:43:07 +1000

    Room Model complete

---
+++ api/models/Rooms.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const RoomSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    maxPeople: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    roomNumbers: [{ number: Number, unavailableDates: { type: [Date] } }],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Room", RoomSchema);
